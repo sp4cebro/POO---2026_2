aba964e08354f9df67d64b15ec1102ece9bd9e1b
sp4cebro <146544681+sp4cebro@users.noreply.github.com>
2026-08-12 13:02:56 +0000

    Alterações no código círculo e a criação do Ponto.java - Recuperação do Conteúdo Perdido de segunda-feira (alistamento militar)

--- 01-CirculosPrj/src/main/java/br/pucrs/poo/Circulo.java
+++ 01-CirculosPrj/src/main/java/br/pucrs/poo/Circulo.java
@@ -1,28 +1,23 @@
 package br.pucrs.poo;
 
 public class Circulo {
-    private double coordX;
-    private double coordY;
+    private Ponto centro;
     private double raio;
 
     public Circulo() {
-        this.coordX = 0;
-        this.coordY = 0;
-        this.raio = 1;
+        this(0, 0, 1);
     }
 
-        public Circulo(double umX, double umY, double umRaio) {
-        this.coordX = umX;
-        this.coordY = umY;
+    public Circulo(double umX, double umY, double umRaio) {
+        centro = new Ponto(umX, umY);
         this.raio = umRaio;
     }
 
-    public void mover(double novoX, double novoY ) {
-        this.coordX = novoX;
-        this.coordY = novoY;
+    public void mover(double novoX, double novoY) {
+        centro.mover(novoX, novoY);
     }
 
-    public void zoom(double fator){
+    public void zoom(double fator) {
         this.raio = this.raio * fator;
     }
 
@@ -33,12 +28,10 @@ public class Circulo {
     }
 
     @Override
-    public String toString(){
-        return "centro: " + this.coordX +
-               ", "       + this.coordY +
-               " raio: "  + this.raio   +
-               " area: "  + this.area();
-
+    public String toString() {
+        return "centro: " + centro.toString() +
+                " raio: " + this.raio +
+                " area: " + this.area();
     }
 
 }
---
+++ 01-CirculosPrj/src/main/java/br/pucrs/poo/Ponto.java
@@ -0,0 +1,21 @@
+package br.pucrs.poo;
+
+public class Ponto{
+    private double coordx;
+    private double coordy;
+
+    public Ponto(doucle coordx, double coordy){
+        this.coordx = coordx;
+        this.coordy = coordy;
+    }
+
+    public void mover(double coordx, double coordy){
+        this.coordx = coordx;
+        this.coordy = coordy;
+    }
+
+    public String toString(){
+        return String.format(
+            "(%.2f, %.2f)", coordx, coordy);
+    }
+}
