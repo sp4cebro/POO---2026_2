19e92bc7af1b4c4ca40c2bbefd46e4605c39c9dd
sp4cebro <146544681+sp4cebro@users.noreply.github.com>
2026-08-17 13:02:21 +0000

    Finalização das partes do código das aulas atrasadas (com correção do que estava errado com Claude)

--- 02-VendasPrj/src/main/java/br/pucrs/poo/ItemVenda.java
+++ 02-VendasPrj/src/main/java/br/pucrs/poo/ItemVenda.java
@@ -4,18 +4,18 @@ public class ItemVenda {
     private int quantidade;
     private Produto produto;
 
-    public ItemVenda(int quantidade, Produto produto){
+    public ItemVenda(int quantidade, Produto produto) {
         this.quantidade = quantidade;
         this.produto = produto;
     }
 
-    public double getValor(){
-        return produto.getPreco();
+    public double getValor() {
+        return quantidade * produto.getPreco();
     }
 
     @Override
-    public String toString(){
-        String saida = "";
+    public String toString() {
+        String saida = quantidade + "x " + produto.getNome() + " = R$ " + getValor();
         return saida;
     }
 }
--- 02-VendasPrj/src/main/java/br/pucrs/poo/Venda.java
+++ 02-VendasPrj/src/main/java/br/pucrs/poo/Venda.java
@@ -1,5 +1,44 @@
 package br.pucrs.poo;
 
 public class Venda {
-    
+    private Cliente cliente;
+    private ItemVenda[] itens;
+    private int qtdItens;
+
+    public Venda(Cliente cliente){
+        this.cliente = cliente;
+        this.itens = new ItemVenda[50];
+        this.qtdItens = 0;
+    }
+
+    public void inserir(Produto prod){
+        inserir(1, prod);
+    }
+
+    public void inserir(int umaQtde, Produto prod){
+        itens[qtdItens] = new ItemVenda(umaQtde, prod);
+        qtdItens++;
+    }
+
+    public double valorTotal(){
+        double total = 0;
+        for(int i = 0; i < qtdItens; i++){
+            total+= itens[i].getValor();
+        }
+        return total;
+    }
+
+    public String getNotaFiscal(){
+        return toString();
+    }
+
+    @Override
+    public String toString(){
+        String saida = "Venda\nCliente: " + cliente.getNome() + "\n";
+        for(int i = 0; i < qtdItens; i++) {
+            saida += itens[i] + "\n";
+        }
+        saida += "Total: R$ " + valorTotal();
+        return saida;
+    }
 }
