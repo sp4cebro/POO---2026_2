630ffe4dd5a7878d94794f263ee6339d1d92d48f
sp4cebro <146544681+sp4cebro@users.noreply.github.com>
2026-08-27 01:26:01 +0000

    Terminada a realização do trabalho e a correção dos erros do terminal foi como eu suspeitava - o package tava reclamando porque eu tinha acabado de criar ele.

--- 05-VendasHeranca/src/main/java/br/pucrs/poo/ClientePJ.java
+++ 05-VendasHeranca/src/main/java/br/pucrs/poo/ClientePJ.java
@@ -1,3 +1,19 @@
-public ClientePJ {
+package br.pucrs.poo;
 
+public class ClientePJ extends Cliente {
+    private String cnpj;
+
+    public ClientePJ(int umCodigo, String umNome, String umCNPJ){
+        super(umCodigo, umNome);
+        this.cnpj = umCNPJ;
+    }
+
+    public String getCNPJ(){ return cnpj;
+    }
+
+    @Override
+    public String toString() {
+        
+        return super.toString() + " [cnpj = " + cnpj + "]";
+    }
 }
--- 05-VendasHeranca/src/main/java/br/pucrs/poo/ProdutoAlcoolico.java
+++ 05-VendasHeranca/src/main/java/br/pucrs/poo/ProdutoAlcoolico.java
@@ -1,3 +1,28 @@
-public class ProdutoAlcoolico {
+package br.pucrs.poo;
 
+//provavelmente fazer alguma alteração direta com a classe Produto para implementar
+//a porcentagem estatica dessa classe
+public class ProdutoAlcoolico extends Produto {
+    private static double taxa_trago = 0.20;
+
+    public ProdutoAlcoolico(int umCodigo, String umNome, double umPreco){
+        super(umCodigo, umNome, umPreco);
+    }
+
+    public double getTaxa(){ return taxa_trago;
+    }
+
+    public static void setTaxa(double novaTaxa){
+        taxa_trago = novaTaxa;
+    }
+
+    public double getPreco(){
+        double precoBase = super.getPreco();
+        return precoBase + (precoBase * taxa_trago);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("[Preço c/ Taxa (%.0f%%): R$ %.2f]", getTaxa() * 100, getPreco());
+    }
 }
